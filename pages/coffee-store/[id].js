def14124c7d43bf68658b05c4aac854440059325
Akram Adib <md.akramuladib3@gmail.com>
2022-09-16 01:27:46 +0600

    changes to commit

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,29 +3,37 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
 import cls from 'classnames'; 
-import coffeeStoresData from '../../data/coffee-stores.json';
+// import coffeeStoresData from '../../data/coffee-stores.json';
 import styles from '../../styles/coffee-store.module.css';
+import { fetchCoffeeStores } from '../../lib/coffee-stores';
 
-
-export function getStaticProps({params}) {
+export async function getStaticProps (staticProps){ 
+    try {
+        const params = staticProps.params
+        const coffeeStores = await fetchCoffeeStores();
+        
+        const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+            return coffeeStore.fsq_id.toString() === params.id;
+    })
+    
     return {
         props: {
-            coffeeStore: coffeeStoresData.find((coffeeStore) => {
-                return coffeeStore.id.toString() === params.id; //dynamic id
-            })
+            coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
         }
     }
-}
-export function getStaticPaths() {
-    const paths = coffeeStoresData.map((coffeeStore) => {
+    } catch (err) {
+        console.log(err)
+    }
+} 
+export async function getStaticPaths() {
+    const coffeeStores = await fetchCoffeeStores();
+    const paths = coffeeStores.map((coffeeStore) => {
         return {
-            params: {
-                id: coffeeStore.id.toString(),
-            }
+            params: { id: coffeeStore.fsq_id.toString() }
         }
     })
     return {
-        paths,
+        paths: paths,
         fallback:true,
     }
 }
@@ -35,11 +43,11 @@ const CoffeeStore = (props) => {
 
     if (router.isFallback) {
         return (
-            <div>Loading...</div>
+            <div><h1>Loading...</h1></div>
         )
     }
 
-    const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+    const { address, name, neighborhood, imgUrl } = props.coffeeStore;
 
     const handleUpvoteButton = () => {
         console.log('handle upvote')
@@ -59,7 +67,7 @@ const CoffeeStore = (props) => {
                         <h1 className={styles.name}>{name}</h1>
                     </div>
                     <Image 
-                    src={imgUrl}
+                    src={imgUrl || 'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'}
                     width={600}
                     height={360}
                     className={styles.storeImg}
@@ -71,10 +79,12 @@ const CoffeeStore = (props) => {
                         <Image src="/static/icons/places.svg" width="24" height="24" />
                         <p className={styles.text}>{address}</p>
                     </div>
+
                     <div className={styles.iconWrapper}>
                         <Image src="/static/icons/nearMe.svg" width="24" height="24" />
-                        <p className={styles.text}>{neighbourhood}</p>
+                        <p className={styles.text}>{neighborhood}</p>
                     </div>
+                
                     <div className={styles.iconWrapper}>
                         <Image src="/static/icons/star.svg" width="24" height="24" />
                         <p className={styles.text}>1</p>
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,17 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Banner from '../components/Banner';
 import Card from '../components/Card';
-import coffeeStoresData from '../data/coffee-stores.json'
+import { fetchCoffeeStores } from '../lib/coffee-stores.js'
+
+// import coffeeStoresData from '../data/coffee-stores.json'
 
 export async function getStaticProps(context) {
-  // console.log('hi getStaticProps')
-  const response = await fetch('https://console.cloud.google.com/search?ll=43.65267326999575,-79.39545615725015&query=coffee stores&client_id=214755918546-lcmulg5jrv2rubv1qhm0u2era7g56hdk.apps.googleusercontent.com&client_secret=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')
-  
-  // https://api.foursquare.com/v2/venues/search?ll=43.65267326999575,-79.39545615725015&query=coffee stores&client_id=BRAY5ALNMO1U0M21XRZMP4O0V3MIUKBQVAKM1JMXBGCBRSTA&client_secret=OQTODYCOEAE3DIM0SEDQISBPYURXC3XS4XMIF04LCUOOYSIF&v=20220911
 
-  const data = await response.json();
-  console.log(data)
+  const coffeeStores = await fetchCoffeeStores();
 
     return {
       props: {
-        coffeeStores: data
+        coffeeStores,
       }
     }
 }
@@ -48,15 +45,15 @@ export default function Home(props) {
 
         {props.coffeeStores.length > 0 && (
         <>
-          <h2 className={styles.heading2}>Toronto Stores :)</h2>
+          <h2 className={styles.heading2}>Toronto Coffee Stores :)</h2>
           <div className={styles.cardLayout}>
             {props.coffeeStores.map((coffeeStore) => {
               return (
                 <Card
-                  key={coffeeStore.id} 
+                  key={coffeeStore.fsq_id} 
                   className={styles.card}
                   name={coffeeStore.name} 
-                  imgUrl={coffeeStore.imgUrl}
+                  imgUrl={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'}
                   href={`/coffee-store/${coffeeStore.id}`} />
               )
             })}
